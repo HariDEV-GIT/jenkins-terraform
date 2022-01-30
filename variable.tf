51f54ad64f54e95e7dfdc79dc424360b6785015f
HariDEV-GIT <harimadhan07@gmail.com>
2022-01-30 12:09:12 +0530

    Modified var file

--- variable.tf
+++ variable.tf
@@ -15,7 +15,7 @@ variable "security_group" {
 
 variable "tag_name" {
   description = "Tag Name of for Ec2 instance"
-  default     = "my-ec2-instance"
+  default     = "Lax-Hari"
 }
 
 variable "ami_id" {
